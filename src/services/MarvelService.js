82018dcb36dd12e0b859861e9bf4b1547d24bf59
eduard7mar <edwardmargaryan@gmail.com>
2023-10-26 21:14:48 +0300

    added spinner and error message

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,23 +1,35 @@
 class MarvelService {
-    _apiBase = "https://gateway.marvel.com:443/v1/public/";
-    _apiKey = "apikey=66936cc068f567cdbd60e79ef19f7a09";
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=66936cc068f567cdbd60e79ef19f7a09";
 
-    getResource = async (url) => {
-        let res = await fetch(url);
-    
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-    
-        return await res.json();
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-    }
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-    }
+    return await res.json();
+  };
+
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter(res));
+  };
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character.',
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  };
 }
 
 export default MarvelService;
