130b49f223fd590686ee6e4447b0ea7af14e83eb
eduard7mar <edwardmargaryan@gmail.com>
2023-11-01 16:49:13 +0200

    final version

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=66936cc068f567cdbd60e79ef19f7a09";
+  _baseOffset = 210;
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -12,12 +13,16 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
-    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-    return res.data.results.map(this._transformCharacter(res));
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
+    );
+    return res.data.results.map(this._transformCharacter);
   };
   getCharacter = async (id) => {
-    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
     return this._transformCharacter(res.data.results[0]);
   };
 
@@ -25,10 +30,13 @@ class MarvelService {
     return {
       id: char.id,
       name: char.name,
-      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character.',
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : "There is no description for this character.",
       thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
+      comics: char.comics.items
     };
   };
 }
